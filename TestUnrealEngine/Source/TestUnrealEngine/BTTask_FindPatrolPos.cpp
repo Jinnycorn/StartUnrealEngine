ebfe0c8525feca435c21e476f81e70e68c78c5d6
Jinnycorn <wldms949494@gmail.com>
2022-07-12 11:37:25 +0900

    Test3

---
+++ TestUnrealEngine/Source/TestUnrealEngine/BTTask_FindPatrolPos.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_FindPatrolPos.h"
+
+UBTTask_FindPatrolPos::UBTTask_FindPatrolPos()
+{
+
+}
+
+EBTNodeResult::Type UBTTask_FindPatrolPos::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+}
---
+++ TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+#include "MyCharacter.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	//코드로 할 때
+	//DefaultPawnClass = AMyCharacter::StaticClass();
+	
+	//블루 프린트로 바꾸기
+	static ConstructorHelpers::FClassFinder<ACharacter>BP_Char(TEXT("Blueprint'/Game/Blueprints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+	{
+		DefaultPawnClass = BP_Char.Class;
+	}
+}
+
